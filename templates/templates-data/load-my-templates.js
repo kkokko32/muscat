a9be4869542d75acfc85a91df182484ca4b67a51
kkokko32 <kkokko32@example.com>
2025-04-18 21:01:57 +0900

    정렬수정

--- templates/templates-data/load-my-templates.js
+++ templates/templates-data/load-my-templates.js
@@ -148,7 +148,7 @@ function applyMasonryLayout() {
         itemSelector: ".template-card",
         columnWidth: ".grid-sizer",
         gutter: 20,
-        fitWidth: true
+        fitWidth: false // ✅ 핵심 변경
       });
     } else {
       window.masonryInstance.reloadItems();
