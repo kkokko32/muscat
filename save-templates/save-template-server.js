d8d2e16598939b1326e428131c1a6ea0ae120f8b
kkokko32 <kkokko32@example.com>
2025-05-05 23:09:14 +0900

    저장수정

--- save-templates/save-template-server.js
+++ save-templates/save-template-server.js
@@ -165,14 +165,17 @@ async function handleSaveTemplate() {
     try {
       const pathname = window.location.pathname;
       const fileName = pathname.substring(pathname.lastIndexOf("/") + 1).split("?")[0];
+      console.log("File Name:", fileName); // 디버깅: 파일명 출력
+
       const id = fileName.replace(".html", "");
+      console.log("Extracted Template ID:", id); // 디버깅: 추출된 templateId 출력
+      
       if (id && id.startsWith("template-")) {
         templateId = id;
       }
     } catch (e) {
       console.warn("templateId 추출 실패, 기본값 사용:", e);
     }
-
 
     const docRef = await addDoc(collection(db, "savedTemplates"), {
       uid: user.uid,
